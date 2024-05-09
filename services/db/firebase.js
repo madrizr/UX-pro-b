bd4b2776d97666f350e4d5b1b1329d21c7044222
Roberto <glasgow525@gmail.com>
2024-05-09 12:17:27 -0400

    fixed project

--- services/db/firebase.js
+++ services/db/firebase.js
@@ -1,14 +1,10 @@
-require('dotenv').config();
-
 const { initializeApp, applicationDefault } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
 
 initializeApp({
-    credential: applicationDefault(),
+    credential: applicationDefault()
 });
 
 const db = getFirestore()
 
-module.exports = {
-    db,
-}
+module.exports = db
